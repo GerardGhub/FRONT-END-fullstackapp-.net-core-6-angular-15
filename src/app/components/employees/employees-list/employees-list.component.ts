257f41eb3db1120c8d0f8783023799c42b5c47b7
GerardGhub <singian28@gmail.com>
2023-01-04 15:22:28 +0800

    Add static list of array in bind to employees component

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.scss']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
+
+employees: Employee[] = [
+  {
+    id: 'sdsdsd23232sfsf',
+    name: 'Gerard',
+    email: 'singian28@gmail.com',
+    phone: 23232323,
+    salary: 1212,
+    department: 'MIS'
+  }
+];
+
+ngOnInit(): void {
+  
+}
+
 
 }
